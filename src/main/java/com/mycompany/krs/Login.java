ba1c209a3992886a610cd237993e113ea574d791
User <cengceng2781@gmail.com>
2026-04-27 15:20:30 +0700

    UPDATE JAM 3 27/04/26

--- src/main/java/com/mycompany/krs/Login.java
+++ src/main/java/com/mycompany/krs/Login.java
@@ -18,7 +18,7 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
-        this.setLocationRelativeTo(null);
+        WindowUtil.setWindow80PercentCenter(this);
     }
 
     /**
