ead7457ea759d8e1e64f5474b9a43ddad5e91c61
User <cengceng2781@gmail.com>
2026-06-08 15:22:03 +0700

    GAS AJALAHHHHH

--- krspbo2lancar (1).sql
+++ krspbo2lancar (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2026 at 09:45 AM
+-- Generation Time: Jun 08, 2026 at 10:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,8 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
 (22, '2603', 'f565d5f66e8237dd7871f28d7390df8f3ba7b9fd17dc4329b54f21cb842ee20c'),
 (23, '26001', '782ff6c9a7d076172dccb71ad7cb72420499b665a958f4fc37331a12d64ca534'),
 (24, '26002', '61b7ccdb512a7d8fcba8112acc60d6c2b84924f5307ce446a436b4b369576a72'),
-(25, 'testadmin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+(25, 'testadmin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),
+(26, '26003', 'b8d3c92f81f5fa2abd8e3b83e0a3a4c496f8337f1f6dcb5b8a0cb809ffbacdb4');
 
 -- --------------------------------------------------------
 
@@ -80,6 +81,7 @@ CREATE TABLE `kelas` (
   `jam` varchar(20) DEFAULT NULL,
   `ruang` varchar(50) DEFAULT NULL,
   `kuota` int(11) DEFAULT NULL,
+  `id_periode` int(11) DEFAULT NULL,
   `kode_mk` varchar(20) DEFAULT NULL,
   `nidn` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -88,8 +90,11 @@ CREATE TABLE `kelas` (
 -- Dumping data for table `kelas`
 --
 
-INSERT INTO `kelas` (`id_kelas`, `hari`, `jam`, `ruang`, `kuota`, `kode_mk`, `nidn`) VALUES
-(8, 'Selasa', '06.00-07.00', '1.05', 39, 'MK1002', '2603');
+INSERT INTO `kelas` (`id_kelas`, `hari`, `jam`, `ruang`, `kuota`, `id_periode`, `kode_mk`, `nidn`) VALUES
+(8, 'Selasa', '06.00-07.00', '1.05', 39, 2, 'MK1002', '2603'),
+(9, 'Selasa', '05.00-07.00', '1.01', 37, 2, 'MK1005', '2602'),
+(10, 'Selasa', '05.00-08.00', '1.02', 39, 2, 'MK1004', '2603'),
+(11, 'Senin', '15.00-17.00', '1.04', 39, 2, 'MK1004', '2601');
 
 -- --------------------------------------------------------
 
@@ -99,15 +104,22 @@ INSERT INTO `kelas` (`id_kelas`, `hari`, `jam`, `ruang`, `kuota`, `kode_mk`, `ni
 
 CREATE TABLE `krs_detail` (
   `id_krs` int(11) NOT NULL,
-  `id_kelas` int(11) NOT NULL
+  `id_kelas` int(11) NOT NULL,
+  `status_detail` varchar(20) NOT NULL DEFAULT 'Menunggu',
+  `catatan_mk` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `krs_detail`
 --
 
-INSERT INTO `krs_detail` (`id_krs`, `id_kelas`) VALUES
-(4, 8);
+INSERT INTO `krs_detail` (`id_krs`, `id_kelas`, `status_detail`, `catatan_mk`) VALUES
+(4, 8, 'Menunggu', NULL),
+(6, 9, 'Menunggu', NULL),
+(7, 10, 'Menunggu', NULL),
+(8, 9, 'Menunggu', NULL),
+(9, 9, 'Disetujui', NULL),
+(9, 11, 'Disetujui', NULL);
 
 -- --------------------------------------------------------
 
@@ -131,7 +143,12 @@ CREATE TABLE `krs_header` (
 
 INSERT INTO `krs_header` (`id_krs`, `total_sks`, `tgl_pengajuan`, `status_validasi`, `catatan_dosen`, `nim`, `id_periode`) VALUES
 (3, 6, '2026-05-21', 'Menunggu', NULL, NULL, 2),
-(4, 3, '2026-05-26', 'Menunggu', NULL, '26001', 4);
+(4, 3, '2026-05-29', 'Disetujui', NULL, '26001', 4),
+(5, 3, '2026-05-29', 'Ditolak', NULL, '26002', 4),
+(6, 3, '2026-05-29', 'Disetujui', 'pengen aja', '26003', 4),
+(7, 4, '2026-06-05', 'Disetujui', NULL, '26001', 2),
+(8, 3, '2026-06-05', 'Disetujui', NULL, '26003', 2),
+(9, 7, '2026-06-05', 'Disetujui', '', '26002', 2);
 
 -- --------------------------------------------------------
 
@@ -141,6 +158,9 @@ INSERT INTO `krs_header` (`id_krs`, `total_sks`, `tgl_pengajuan`, `status_valida
 
 CREATE TABLE `mahasiswa` (
   `nim` varchar(20) NOT NULL,
+  `angkatan` year(4) DEFAULT NULL,
+  `semester_aktif` tinyint(3) UNSIGNED DEFAULT 1,
+  `status_mahasiswa` varchar(20) DEFAULT 'Aktif',
   `nama_mhs` varchar(100) NOT NULL,
   `alamat` text DEFAULT NULL,
   `id_prodi` int(11) DEFAULT NULL,
@@ -152,9 +172,10 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama_mhs`, `alamat`, `id_prodi`, `id_dosen_wali`, `id_user`) VALUES
-('26001', 'William Martin Tiono', 'asd', 3, '2602', 23),
-('26002', 'Christ', '123123123', 2, '2603', 24);
+INSERT INTO `mahasiswa` (`nim`, `angkatan`, `semester_aktif`, `status_mahasiswa`, `nama_mhs`, `alamat`, `id_prodi`, `id_dosen_wali`, `id_user`) VALUES
+('26001', NULL, 1, 'Aktif', 'William Martin Tiono', 'asd', 3, '2602', 23),
+('26002', NULL, 1, 'Aktif', 'Christ', '123123123', 2, '2603', 24),
+('26003', NULL, 1, 'Aktif', 'Pris', 'asd', 4, '2601', 26);
 
 -- --------------------------------------------------------
 
@@ -201,6 +222,8 @@ CREATE TABLE `mata_kuliah_prasyarat` (
 CREATE TABLE `nilai` (
   `id_nilai` int(11) NOT NULL,
   `grade` varchar(5) DEFAULT NULL,
+  `angka_nilai` decimal(5,2) DEFAULT NULL,
+  `id_periode` int(11) DEFAULT NULL,
   `nim` varchar(20) DEFAULT NULL,
   `kode_mk` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -213,7 +236,10 @@ CREATE TABLE `nilai` (
 
 CREATE TABLE `pembayaran` (
   `id_bayar` int(11) NOT NULL,
-  `status_Lunas` varchar(20) DEFAULT NULL,
+  `status_lunas` varchar(20) DEFAULT 'Belum Lunas',
+  `tgl_bayar` date DEFAULT NULL,
+  `nominal` decimal(12,0) DEFAULT NULL,
+  `keterangan` varchar(200) DEFAULT NULL,
   `id_periode` int(11) DEFAULT NULL,
   `nim` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -236,9 +262,11 @@ CREATE TABLE `periode` (
 --
 
 INSERT INTO `periode` (`id_periode`, `tahun_ajaran`, `semester`, `status_krs`) VALUES
-(2, '2024/2025', 'Ganjil', 'Tutup'),
+(2, '2024/2025', 'Ganjil', 'Buka'),
 (3, '2025/2026', 'Ganjil', 'Tutup'),
-(4, '2026/2027', 'Ganjil', 'Buka');
+(4, '2026/2027', 'Ganjil', 'Tutup'),
+(5, '2024/2025', 'Genap', 'Tutup'),
+(6, '2025/2026', 'Genap', 'Tutup');
 
 -- --------------------------------------------------------
 
@@ -285,7 +313,8 @@ ALTER TABLE `dosen`
 ALTER TABLE `kelas`
   ADD PRIMARY KEY (`id_kelas`),
   ADD KEY `kode_mk` (`kode_mk`),
-  ADD KEY `nidn` (`nidn`);
+  ADD KEY `nidn` (`nidn`),
+  ADD KEY `fk_kelas_periode` (`id_periode`);
 
 --
 -- Indexes for table `krs_detail`
@@ -332,14 +361,17 @@ ALTER TABLE `mata_kuliah_prasyarat`
 --
 ALTER TABLE `nilai`
   ADD PRIMARY KEY (`id_nilai`),
+  ADD UNIQUE KEY `uq_nilai` (`nim`,`kode_mk`,`id_periode`),
   ADD KEY `nim` (`nim`),
-  ADD KEY `kode_mk` (`kode_mk`);
+  ADD KEY `kode_mk` (`kode_mk`),
+  ADD KEY `fk_nilai_periode` (`id_periode`);
 
 --
 -- Indexes for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
   ADD PRIMARY KEY (`id_bayar`),
+  ADD UNIQUE KEY `uq_pembayaran` (`nim`,`id_periode`),
   ADD KEY `id_periode` (`id_periode`),
   ADD KEY `nim` (`nim`);
 
@@ -363,25 +395,31 @@ ALTER TABLE `prodi`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `krs_header`
 --
 ALTER TABLE `krs_header`
-  MODIFY `id_krs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_krs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `mata_kuliah_prasyarat`
+--
+ALTER TABLE `mata_kuliah_prasyarat`
+  MODIFY `id_prasyarat` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `periode`
 --
 ALTER TABLE `periode`
-  MODIFY `id_periode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_periode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -399,7 +437,8 @@ ALTER TABLE `dosen`
 --
 ALTER TABLE `kelas`
   ADD CONSTRAINT `fk_kelas_dosen` FOREIGN KEY (`nidn`) REFERENCES `dosen` (`nidn`) ON DELETE SET NULL,
-  ADD CONSTRAINT `fk_kelas_mk` FOREIGN KEY (`kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`) ON DELETE SET NULL;
+  ADD CONSTRAINT `fk_kelas_mk` FOREIGN KEY (`kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_kelas_periode` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id_periode`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `krs_detail`
@@ -435,7 +474,8 @@ ALTER TABLE `mata_kuliah_prasyarat`
 --
 ALTER TABLE `nilai`
   ADD CONSTRAINT `fk_nilai_mhs` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE SET NULL,
-  ADD CONSTRAINT `fk_nilai_mk` FOREIGN KEY (`kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`) ON DELETE SET NULL;
+  ADD CONSTRAINT `fk_nilai_mk` FOREIGN KEY (`kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_nilai_periode` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id_periode`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `pembayaran`
